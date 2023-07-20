1d31f80810dc621c87ab0703317650cb25106298
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-07-20 15:34:55 +0000

    create controller for API and rails app

--- app/controllers/send_messages_controller.rb
+++ app/controllers/send_messages_controller.rb
@@ -1,69 +1,94 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class SendMessagesController < ApplicationController
-  before_action :set_send_message, only: %i[show edit update destroy]
+  skip_before_action :verify_authenticity_token
 
-  # GET /send_messages or /send_messages.json
-  def index
-    @send_messages = SendMessage.all
-  end
+  MAIN_URL = 'https://mock-text-provider.parentsquare.com/'.freeze
 
-  # GET /send_messages/1 or /send_messages/1.json
-  def show; end
+  PROVIDER_1_URL = 'provider1'.freeze
+  PROVIDER_2_URL = 'provider2'.freeze
 
-  # GET /send_messages/new
-  def new
-    @send_message = SendMessage.new
-  end
-
-  # GET /send_messages/1/edit
-  def edit; end
-
-  # POST /send_messages or /send_messages.json
   def create
-    @send_message = SendMessage.new(send_message_params)
+    # Send back an id when a message is created
+    to_number = create_message_params[:number]
+    message = create_message_params[:message]
 
-    respond_to do |format|
-      if @send_message.save
-        format.html { redirect_to send_message_url(@send_message), notice: 'Send message was successfully created.' }
-        format.json { render :show, status: :created, location: @send_message }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @send_message.errors, status: :unprocessable_entity }
-      end
+    payload = {
+      to_number:,
+      message:,
+      callback_url: 'https://7411-197-251-147-157.ngrok-free.app/delivery_status'
+    }
+
+    # Determine which provider to use based on the weighting
+    provider_url = weighted_provider_url
+
+    # Make the request to the external service (Provider 1 or Provider 2)
+    attempt = make_request_to_provider(MAIN_URL + provider_url, payload)
+
+    code_status = JSON.parse(attempt[:code])
+    message_id = JSON.parse(attempt[:body])['message_id']
+
+    if code_status == 200
+      @new_message = SendMessage.create(
+        to_number:,
+        message:,
+        attempt_status: '',
+        message_id: message_id.to_s
+      )
+      @new_message.save
+    elsif code_status == 500
+      redirect_to root_path, notice: 'System is offline now. Please try again later!'
     end
   end
 
-  # PATCH/PUT /send_messages/1 or /send_messages/1.json
-  def update
-    respond_to do |format|
-      if @send_message.update(send_message_params)
-        format.html { redirect_to send_message_url(@send_message), notice: 'Send message was successfully updated.' }
-        format.json { render :show, status: :ok, location: @send_message }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @send_message.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  def delivery_status
+    # Extract data from the webhook request
+    message_id = params[:message_id]
+    status = params[:status]
 
-  # DELETE /send_messages/1 or /send_messages/1.json
-  def destroy
-    @send_message.destroy
-
-    respond_to do |format|
-      format.html { redirect_to send_messages_url, notice: 'Send message was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    message = SendMessage.find_by(message_id:)
+    message&.update(attempt_status: status.to_s)
+    head :ok
   end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_send_message
-    @send_message = SendMessage.find(params[:id])
+  def create_message_params
+    params.permit(:to_number, :message, :message_id, :number, :status)
   end
 
-  # Only allow a list of trusted parameters through.
-  def send_message_params
-    params.fetch(:send_message, {})
+  def weighted_provider_url
+    rand <= 0.3 ? PROVIDER_1_URL : PROVIDER_2_URL
+  end
+
+  def make_request_to_provider(url, payload)
+    # Prepare the request data
+    uri = URI.parse(url)
+    headers = { 'Content-Type' => 'application/json' }
+
+    # Make the request to the external service
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = (uri.scheme == 'https')
+    request = Net::HTTP::Post.new(uri.path, headers)
+    request.body = payload.to_json
+
+    response = http.request(request)
+
+    # Return both response.body and response.code
+    { body: response.body, code: response.code }
+  rescue Net::HTTPServerError
+    # Retry through the other provider if one is down
+    if url == PROVIDER_1_URL
+      make_request_to_provider(MAIN_URL + PROVIDER_2_URL,
+                               payload)
+    else
+      make_request_to_provider(MAIN_URL + PROVIDER_1_URL,
+                               payload)
+    end
+  rescue StandardError
+    # Handle other errors or if no provider is available
+    { body: 'Message failed: No provider available', code: '500' }
   end
 end
