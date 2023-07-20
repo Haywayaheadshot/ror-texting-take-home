46160169350c84c4cdb5f88c4965f5ef83e58141
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-07-20 15:37:06 +0000

    include delivery_status and create_messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,22 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  namespace :dev do
-    resources :send_messages
+  # # Defines the root path route ("/")
+  root "send_messages#new"
+  resources :send_messages
 
+  post '/send_messages/create', to: 'send_messages#create', as: 'create_messages'
+
+  post '/delivery_status', to: 'send_messages#delivery_status', as: 'update_status'
+  namespace :dev do
     # API endpoint for posting to provider 1
-    post "dev/provider1", to: "dev/send_messages#provider_one"
+    post "provider1", to: "send_messages#provider_one"
+
+    # API endpoint for posting to check for delivery status
+    post 'delivery_status', to: 'send_messages#delivery_status'
+    
+    # Defines the root path route ("/")
+    root to: "send_messages#index"
+    resources :send_messages
   end
 end
