b7144adc3bd34ae1a080e6003e8b8ef91fabf3bf
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-07-20 15:37:38 +0000

    update tables

--- db/migrate/20230719065130_create_send_messages.rb
+++ db/migrate/20230719065130_create_send_messages.rb
@@ -1,9 +1,10 @@
 class CreateSendMessages < ActiveRecord::Migration[7.0]
   def change
     create_table :send_messages do |t|
-      t.integer :to_number
+      t.bigint :to_number
       t.string :message
-      t.string :callback_url
+      t.string :attempt_status
+      t.string :message_id
 
       t.timestamps
     end
