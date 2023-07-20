dfcac66f912417df895f78d19b151ac4fe4e6cfb
Abubakar Ummar <haywayaheadshotstrategy.fx@gmail.com>
2023-07-20 15:37:52 +0000

    update schema file

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_19_065130) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_080738) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "send_messages", force: :cascade do |t|
-    t.integer "to_number"
+    t.bigint "to_number"
     t.string "message"
-    t.string "callback_url"
+    t.string "attempt_status"
+    t.string "message_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uuid"
+    t.index ["uuid"], name: "index_send_messages_on_uuid"
   end
 
 end
