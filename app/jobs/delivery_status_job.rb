cb8de9d97c210c649247c2f5b59510446632f68f
Abubakar <haywayaheadshotstrategy.fx@gmail.com>
2023-08-21 14:47:29 +0000

    Delete delivery_status method logic does not make sense

--- app/jobs/delivery_status_job.rb
+++ app/jobs/delivery_status_job.rb
@@ -1,13 +1,7 @@
 class DeliveryStatusJob < ApplicationJob
   queue_as :default
 
-  def perform(*_args)
-    # Extract data from the webhook request
-    message_id = params[:message_id]
-    status = params[:status]
-
-    message = SendMessage.find_by(message_id:)
-    message&.update(attempt_status: status.to_s)
-    head :ok
+  def perform(*args)
+    # Do something later
   end
 end
